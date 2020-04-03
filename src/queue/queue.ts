5b57c0f1df1aa8f30dc4c3861732f517bc17b58f
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2020-04-03 21:42:36 +0300

    Document Queue and Stack

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,14 +1,36 @@
+/**
+ * Queue implementation in TypeScript
+ * @class Queue
+ * @template T
+ */
 class Queue<T> {
+    /**
+     * Holds items in the Stack
+     * @property {T[]} items
+     */
     items: T[];
 
+    /**
+     * Creates a new instance of Queue
+     * @param {T[]} items
+     */
     constructor(...items: T[]) {
         this.items = items;
     }
 
+    /**
+     * Adds items to the end of the queue array
+     * @param {T} items
+     * @returns {number} Lenght of the queue array
+     */
     enqueue(...items: T[]): number {
         return this.items.unshift(...items);
     }
 
+    /**
+     * Removes the first item of the queue array
+     * @returns {T|undefined} Removed item
+     */
     dequeue(): T | undefined {
         return this.items.pop();
     }
--- src/stack/stack.ts
+++ src/stack/stack.ts
@@ -1,14 +1,36 @@
+/**
+ * Stack implementation in TypeScript
+ * @class Stack
+ * @template T
+ */
 class Stack<T> {
+    /**
+     * Holds items in the Stack
+     * @property {T[]} items
+     */
     items: T[];
 
+    /**
+     * Creates a new instance of Stack
+     * @param {T[]} items
+     */
     constructor(...items: T[]) {
         this.items = [...items];
     }
 
+    /**
+     * Adds items to the top of the stack array
+     * @param {T[]} items
+     * @returns {number} Lenght of the stack array
+     */
     push(...items: T[]): number {
         return this.items.push(...items);
     }
 
+    /**
+     * Removes the last item from the stack array
+     * @returns {T|undefined} Removed item
+     */
     pop(): T | undefined {
         return this.items.pop();
     }
