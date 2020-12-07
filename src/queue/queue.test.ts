92441e43839458ec2389a930ef1bb8a948f000ba
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2020-12-07 23:15:22 +0200

    Unify test names

--- src/queue/queue.test.ts
+++ src/queue/queue.test.ts
@@ -1,13 +1,13 @@
 import Queue from "./queue";
 
 describe("Queue", () => {
-    it("Should enqueue items to the left", () => {
+    it("should enqueue items to the left", () => {
         const queue = new Queue<number>(4, 5);
         expect(queue.enqueue(1, 2, 3)).toBe(5);
         expect(queue.items).toEqual([1, 2, 3, 4, 5]);
     });
 
-    it("Should dequeue items from the right", () => {
+    it("should dequeue items from the right", () => {
         const queue = new Queue<number>(1, 2, 3);
         expect(queue.dequeue()).toBe(3);
         expect(queue.items).toEqual([1, 2]);
--- src/stack/stack.test.ts
+++ src/stack/stack.test.ts
@@ -1,13 +1,13 @@
 import Stack from "./stack";
 
 describe("Stack", () => {
-    it("Should push items to top of stack", () => {
+    it("should push items to top of stack", () => {
         const stack = new Stack<number>(4, 5);
         expect(stack.push(1, 2, 3)).toBe(5);
         expect(stack.items).toEqual([4, 5, 1, 2, 3]);
     });
 
-    it("Should pop item from top of stack", () => {
+    it("should pop item from top of stack", () => {
         const stack = new Stack<number>(1, 2, 3);
         expect(stack.pop()).toBe(3);
         expect(stack.items).toEqual([1, 2]);
